73b880fdc04dafd70fe30e63b24ec7989eade10a
zhengtao <1263641416@qq.com>
2024-11-12 00:59:08 +0800

    新增菜品

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,18 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import org.springframework.transaction.annotation.Transactional;
+
+public class DishServiceImpl {
+  /**
+   * 新增菜品和对应的口味数据
+   *
+   * @param dishDTO
+   */
+  @Transactional
+  public void saveWithFlavor(DishDTO dishDTO) {
+
+  }
+
+  ;
+}
