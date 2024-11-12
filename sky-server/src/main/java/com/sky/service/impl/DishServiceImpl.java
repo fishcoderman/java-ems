6f2246d8a761bb70b3476c85d8f0027ccb7b7bc5
zhengtao <1263641416@qq.com>
2024-11-12 20:09:31 +0800

    新增菜品

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,9 +1,28 @@
 package com.sky.service.impl;
 
 import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-public class DishServiceImpl {
+import java.util.List;
+
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+
+  @Autowired
+  private DishMapper dishMapper;
+  @Autowired
+  private DishFlavorMapper dishFlavorMapper;
+
   /**
    * 新增菜品和对应的口味数据
    *
@@ -11,8 +30,21 @@ public class DishServiceImpl {
    */
   @Transactional
   public void saveWithFlavor(DishDTO dishDTO) {
+    // 向菜品插入一条数据
+    Dish dish = new Dish();
+    BeanUtils.copyProperties(dishDTO, dish);
+    dishMapper.insert(dish);
 
+    // 获取id
+    Long dishId = dish.getId();
+
+    // 向口味表插入数据
+    List<DishFlavor> dishFlavors = dishDTO.getFlavors();
+    if (dishFlavors != null && dishFlavors.size() > 0) {
+      dishFlavors.forEach(item -> {
+        item.setDishId(dishId);
+      });
+      dishFlavorMapper.insertBatch(dishFlavors);
+    }
   }
-
-  ;
 }
